b0a299d9060fb360ce7d04b8b037225892317635
Gg86249731 <nicolas.schleicher@jala.university>
2024-02-24 21:46:51 -0300

    feature/CrossCutting

---
+++ SkillMasteryAPI/src/Core/SkillMasteryAPI.Application/CrossCutting/EmptyIdException.cs
@@ -0,0 +1,17 @@
+﻿namespace SkillMasteryAPI.Application.CrossCutting;
+
+public class EmptyIdException : ArgumentException
+{
+    public EmptyIdException(string message) : base(message)
+    {
+    }
+
+    public static void ThrowIfIdZero(
+        int id, string? message = null, string? elementName = null)
+    {
+        if(id == 0)
+        {
+            throw new EmptyIdException(message ?? $"{elementName} Id cannot be empty");
+        }
+    }
+}
